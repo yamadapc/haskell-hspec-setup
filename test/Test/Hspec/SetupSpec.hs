4518a0d6921c767a59a7ac58ff5f3dd25dc2e2c6
yamadapc <tacla.yamada@gmail.com>
2016-08-18 21:47:06 -0300

    Update for running tests on containers
    Vendors language-dockerfile so we don't have to wait for stackage or
    hackage.

--- test/Test/Hspec/SetupSpec.hs
+++ test/Test/Hspec/SetupSpec.hs
@@ -1,8 +1,17 @@
 module Test.Hspec.SetupSpec
   where
 
+import           Language.Dockerfile
 import           System.Process
+
 import           Test.Hspec
+
+newProjectImage :: EDockerfileM ()
+newProjectImage = do
+    from "haskell:8"
+    run "mkdir /app"
+    workdir "/app"
+    run "cabal init --minimal"
 
 spec :: Spec
 spec =
