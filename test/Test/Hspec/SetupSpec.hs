f56694ec8ec5f0261aff5951f81a59220304149b
yamadapc <tacla.yamada@gmail.com>
2016-08-18 21:01:13 -0300

    Seed our actual tests

---
+++ test/Test/Hspec/SetupSpec.hs
@@ -0,0 +1,10 @@
+module Test.Hspec.SetupSpec
+  where
+
+import           System.Process
+import           Test.Hspec
+
+spec :: Spec
+spec =
+    describe "given an empty cabal project" $ do
+        undefined
